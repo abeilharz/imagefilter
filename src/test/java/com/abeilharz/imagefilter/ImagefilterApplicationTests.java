729a58e7d49d7a60789cf7ae8771f2f9d73ec2d3
Andreas Beilharz <andreas.beilharz@gmx.net>
2023-04-09 11:35:02 +0200

    Adding the spring boot structure

---
+++ src/test/java/com/abeilharz/imagefilter/ImagefilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeilharz.imagefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagefilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
